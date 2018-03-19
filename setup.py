52e74e92922574f3f0170b55cc4e9d17f60df0d5
Alexey Stepanov <penguinolog@gmail.com>
2018-03-19 11:15:21 +0100

    CD start

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ requires_optimization = [
     _extension('exec_helpers.exceptions'),
     _extension('exec_helpers.exec_result'),
     _extension('exec_helpers.proc_enums'),
+    _extension('exec_helpers._ssh_client_base'),
     _extension('exec_helpers.ssh_client'),
     _extension('exec_helpers.subprocess_runner'),
     _extension('exec_helpers.__init__'),
