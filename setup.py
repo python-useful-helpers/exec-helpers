2a3fac69069286a884578f01903fe76fb10b2a35
Alexey Stepanov <penguinolog@gmail.com>
2018-06-08 10:58:06 +0200

    Move URLs to setup.cfg
    This attributes are parsed by modern setuptools only, so reduce warnings
    
    Signed-off-by: Alexey Stepanov <penguinolog@gmail.com>

--- setup.py
+++ setup.py
@@ -261,10 +261,6 @@ setup_args = dict(
         for name, email in variables['__maintainers__'].items()
     ),
     url=variables['__url__'],
-    project_urls={
-        "Bug Tracker": "https://github.com/python-useful-helpers/exec-helpers/issues",
-        "Documentation": "https://exec-helpers.readthedocs.io/",
-    },
     version=variables['__version__'],
     license=variables['__license__'],
     description=variables['__description__'],
