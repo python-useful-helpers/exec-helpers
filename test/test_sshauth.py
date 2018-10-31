a2c2361af469c66cb84b4ea557ba233f876a2013
Alexey Stepanov <penguinolog@gmail.com>
2018-10-31 13:18:01 +0100

    Fix ExecResult compare: allow subclassing. Re-raise auth errors ASAP. (#99)
    * Make tests faster.
    * Bump to 3.1.1
    
    (cherry picked from commit 2a64bbf44c5764ce3413c8db3871ef203aa513fd)
    Signed-off-by: Alexey Stepanov <penguinolog@gmail.com>

--- test/test_sshauth.py
+++ test/test_sshauth.py
@@ -1,6 +1,4 @@
 #    Copyright 2018 Alexey Stepanov aka penguinolog.
-
-#    Copyright 2016 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
