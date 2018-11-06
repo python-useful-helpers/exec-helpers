03349d1818b9bf199920af33185e6e2c62548515
Alexey Stepanov <penguinolog@gmail.com>
2018-11-06 13:58:58 +0100

    Fix SSHClient: stdin processing, missed command in result for parallel (#100)
    Port part of tests to pytest: better use-cases coverage
    
    (cherry picked from commit dec689bf089a5acd0f1cee5aad259651beac73d0)
    Signed-off-by: Alexey Stepanov <penguinolog@gmail.com>

---
+++ test/test_sftp.py
@@ -0,0 +1,287 @@
+#    Copyright 2018 Alexey Stepanov aka penguinolog.
+
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# pylint: disable=no-self-use
+
+import os
+import posixpath
+import stat
+import unittest
+
+import mock
+import paramiko
+
+import exec_helpers
+
+
+host = "127.0.0.1"
+port = 22
+username = "user"
+password = "pass"
+
+
+@mock.patch("logging.getLogger", autospec=True)
+@mock.patch("paramiko.AutoAddPolicy", autospec=True, return_value="AutoAddPolicy")
+@mock.patch("paramiko.SSHClient", autospec=True)
+class TestSftp(unittest.TestCase):
+    def tearDown(self):
+        with mock.patch("warnings.warn"):
+            exec_helpers.SSHClient._clear_cache()
+
+    @staticmethod
+    def prepare_sftp_file_tests(client):
+        _ssh = mock.Mock()
+        client.return_value = _ssh
+        _sftp = mock.Mock()
+        open_sftp = mock.Mock(parent=_ssh, return_value=_sftp)
+        _ssh.attach_mock(open_sftp, "open_sftp")
+
+        # noinspection PyTypeChecker
+        ssh = exec_helpers.SSHClient(
+            host=host, port=port, auth=exec_helpers.SSHAuth(username=username, password=password)
+        )
+        return ssh, _sftp
+
+    def test_exists(self, client, *args):
+        ssh, _sftp = self.prepare_sftp_file_tests(client)
+        lstat = mock.Mock()
+        _sftp.attach_mock(lstat, "lstat")
+        dst = "/etc"
+
+        # noinspection PyTypeChecker
+        result = ssh.exists(dst)
+        self.assertTrue(result)
+        lstat.assert_called_once_with(dst)
+
+        # Negative scenario
+        lstat.reset_mock()
+        lstat.side_effect = IOError
+
+        # noinspection PyTypeChecker
+        result = ssh.exists(dst)
+        self.assertFalse(result)
+        lstat.assert_called_once_with(dst)
+
+    def test_stat(self, client, *args):
+        ssh, _sftp = self.prepare_sftp_file_tests(client)
+        stat = mock.Mock()
+        _sftp.attach_mock(stat, "stat")
+        stat.return_value = paramiko.sftp_attr.SFTPAttributes()
+        stat.return_value.st_size = 0
+        stat.return_value.st_uid = 0
+        stat.return_value.st_gid = 0
+        dst = "/etc/passwd"
+
+        # noinspection PyTypeChecker
+        result = ssh.stat(dst)
+        self.assertEqual(result.st_size, 0)
+        self.assertEqual(result.st_uid, 0)
+        self.assertEqual(result.st_gid, 0)
+
+    def test_isfile(self, client, *args):
+        class Attrs:
+            def __init__(self, mode):
+                self.st_mode = mode
+
+        ssh, _sftp = self.prepare_sftp_file_tests(client)
+        lstat = mock.Mock()
+        _sftp.attach_mock(lstat, "lstat")
+        lstat.return_value = Attrs(stat.S_IFREG)
+        dst = "/etc/passwd"
+
+        # noinspection PyTypeChecker
+        result = ssh.isfile(dst)
+        self.assertTrue(result)
+        lstat.assert_called_once_with(dst)
+
+        # Negative scenario
+        lstat.reset_mock()
+        lstat.return_value = Attrs(stat.S_IFDIR)
+
+        # noinspection PyTypeChecker
+        result = ssh.isfile(dst)
+        self.assertFalse(result)
+        lstat.assert_called_once_with(dst)
+
+        lstat.reset_mock()
+        lstat.side_effect = IOError
+
+        # noinspection PyTypeChecker
+        result = ssh.isfile(dst)
+        self.assertFalse(result)
+        lstat.assert_called_once_with(dst)
+
+    def test_isdir(self, client, *args):
+        class Attrs:
+            def __init__(self, mode):
+                self.st_mode = mode
+
+        ssh, _sftp = self.prepare_sftp_file_tests(client)
+        lstat = mock.Mock()
+        _sftp.attach_mock(lstat, "lstat")
+        lstat.return_value = Attrs(stat.S_IFDIR)
+        dst = "/etc/passwd"
+
+        # noinspection PyTypeChecker
+        result = ssh.isdir(dst)
+        self.assertTrue(result)
+        lstat.assert_called_once_with(dst)
+
+        # Negative scenario
+        lstat.reset_mock()
+        lstat.return_value = Attrs(stat.S_IFREG)
+
+        # noinspection PyTypeChecker
+        result = ssh.isdir(dst)
+        self.assertFalse(result)
+        lstat.assert_called_once_with(dst)
+
+        lstat.reset_mock()
+        lstat.side_effect = IOError
+        # noinspection PyTypeChecker
+        result = ssh.isdir(dst)
+        self.assertFalse(result)
+        lstat.assert_called_once_with(dst)
+
+    @mock.patch("exec_helpers.ssh_client.SSHClient.exists")
+    @mock.patch("exec_helpers.ssh_client.SSHClient.execute")
+    def test_mkdir(self, execute, exists, *args):
+        exists.side_effect = [False, True]
+
+        dst = "~/tst dir"
+        escaped_dst = r"~/tst\ dir"
+
+        # noinspection PyTypeChecker
+        ssh = exec_helpers.SSHClient(
+            host=host, port=port, auth=exec_helpers.SSHAuth(username=username, password=password)
+        )
+
+        # Path not exists
+        # noinspection PyTypeChecker
+        ssh.mkdir(dst)
+        exists.assert_called_once_with(dst)
+        execute.assert_called_once_with("mkdir -p {}\n".format(escaped_dst))
+
+        # Path exists
+        exists.reset_mock()
+        execute.reset_mock()
+
+        # noinspection PyTypeChecker
+        ssh.mkdir(dst)
+        exists.assert_called_once_with(dst)
+        execute.assert_not_called()
+
+    @mock.patch("exec_helpers.ssh_client.SSHClient.execute")
+    def test_rm_rf(self, execute, *args):
+        dst = "~/tst"
+
+        # noinspection PyTypeChecker
+        ssh = exec_helpers.SSHClient(
+            host=host, port=port, auth=exec_helpers.SSHAuth(username=username, password=password)
+        )
+
+        # Path not exists
+        # noinspection PyTypeChecker
+        ssh.rm_rf(dst)
+        execute.assert_called_once_with("rm -rf {}".format(dst))
+
+    def test_open(self, client, *args):
+        ssh, _sftp = self.prepare_sftp_file_tests(client)
+        fopen = mock.Mock(return_value=True)
+        _sftp.attach_mock(fopen, "open")
+
+        dst = "/etc/passwd"
+        mode = "r"
+        # noinspection PyTypeChecker
+        result = ssh.open(dst)
+        fopen.assert_called_once_with(dst, mode)
+        self.assertTrue(result)
+
+    @mock.patch("exec_helpers.ssh_client.logger", autospec=True)
+    @mock.patch("exec_helpers.ssh_client.SSHClient.exists")
+    @mock.patch("os.path.exists", autospec=True)
+    @mock.patch("exec_helpers.ssh_client.SSHClient.isdir")
+    @mock.patch("os.path.isdir", autospec=True)
+    def test_download(self, isdir, remote_isdir, exists, remote_exists, logger, client, policy, _logger):
+        ssh, _sftp = self.prepare_sftp_file_tests(client)
+        isdir.return_value = True
+        exists.side_effect = [True, False, False]
+        remote_isdir.side_effect = [False, False, True]
+        remote_exists.side_effect = [True, False, False]
+
+        dst = "/etc/environment"
+        target = "/tmp/environment"
+        # noinspection PyTypeChecker
+        result = ssh.download(destination=dst, target=target)
+        self.assertTrue(result)
+        isdir.assert_called_once_with(target)
+        exists.assert_called_once_with(posixpath.join(target, os.path.basename(dst)))
+        remote_isdir.assert_called_once_with(dst)
+        remote_exists.assert_called_once_with(dst)
+        _sftp.assert_has_calls((mock.call.get(dst, posixpath.join(target, os.path.basename(dst))),))
+
+        # Negative scenarios
+        # noinspection PyTypeChecker
+        result = ssh.download(destination=dst, target=target)
+        self.assertFalse(result)
+
+        # noinspection PyTypeChecker
+        ssh.download(destination=dst, target=target)
+
+    @mock.patch("exec_helpers.ssh_client.SSHClient.isdir")
+    @mock.patch("os.path.isdir", autospec=True)
+    def test_upload_file(self, isdir, remote_isdir, client, *args):
+        ssh, _sftp = self.prepare_sftp_file_tests(client)
+        isdir.return_value = False
+        remote_isdir.return_value = False
+        target = "/etc/environment"
+        source = "/tmp/environment"
+
+        # noinspection PyTypeChecker
+        ssh.upload(source=source, target=target)
+        isdir.assert_called_once_with(source)
+        remote_isdir.assert_called_once_with(target)
+        _sftp.assert_has_calls((mock.call.put(source, target),))
+
+    @mock.patch("exec_helpers.ssh_client.SSHClient.exists")
+    @mock.patch("exec_helpers.ssh_client.SSHClient.mkdir")
+    @mock.patch("os.walk")
+    @mock.patch("exec_helpers.ssh_client.SSHClient.isdir")
+    @mock.patch("os.path.isdir", autospec=True)
+    def test_upload_dir(self, isdir, remote_isdir, walk, mkdir, exists, client, *args):
+        ssh, _sftp = self.prepare_sftp_file_tests(client)
+        isdir.return_value = True
+        remote_isdir.return_value = True
+        exists.return_value = True
+        target = "/etc"
+        source = "/tmp/bash"
+        filename = "bashrc"
+        walk.return_value = ((source, "", [filename]),)
+        expected_path = posixpath.join(target, os.path.basename(source))
+        expected_file = posixpath.join(expected_path, filename)
+
+        # noinspection PyTypeChecker
+        ssh.upload(source=source, target=target)
+        isdir.assert_called_once_with(source)
+        remote_isdir.assert_called_once_with(target)
+        mkdir.assert_called_once_with(expected_path)
+        exists.assert_called_once_with(expected_file)
+        _sftp.assert_has_calls(
+            (
+                mock.call.unlink(expected_file),
+                mock.call.put(os.path.normpath(os.path.join(source, filename)), expected_file),
+            )
+        )
---
+++ test/test_ssh_client_execute_throw_host.py
@@ -0,0 +1,183 @@
+#    Copyright 2018 Alexey Stepanov aka penguinolog.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import base64
+import typing
+
+import mock
+import pytest
+
+import exec_helpers
+
+
+class FakeFileStream:
+    """Mock-like object for stream emulation."""
+
+    def __init__(self, *args):
+        self.__src = list(args)
+        self.closed = False
+        self.channel = None
+
+    def __iter__(self):
+        """Normally we iter over source."""
+        for _ in range(len(self.__src)):
+            yield self.__src.pop(0)
+
+    def fileno(self):
+        return hash(tuple(self.__src))
+
+    def close(self):
+        """We enforce close."""
+        self.closed = True
+
+
+def read_stream(stream: FakeFileStream) -> typing.Tuple[bytes, ...]:
+    return tuple([line for line in stream])
+
+
+host = "127.0.0.1"
+port = 22
+username = "user"
+password = "pass"
+
+command = "ls ~\nline 2\nline 3\nline с кирилицей"
+command_log = "Executing command:\n{!r}\n".format(command.rstrip())
+stdout_src = (b" \n", b"2\n", b"3\n", b" \n")
+stderr_src = (b" \n", b"0\n", b"1\n", b" \n")
+encoded_cmd = base64.b64encode("{}\n".format(command).encode("utf-8")).decode("utf-8")
+
+print_stdin = 'read line; echo "$line"'
+default_timeout = 60 * 60  # 1 hour
+
+
+@pytest.fixture
+def auto_add_policy(mocker):
+    return mocker.patch("paramiko.AutoAddPolicy", return_value="AutoAddPolicy")
+
+
+@pytest.fixture
+def paramiko_ssh_client(mocker):
+    mocker.patch("time.sleep")
+    return mocker.patch("paramiko.SSHClient")
+
+
+@pytest.fixture
+def chan_makefile():
+    class MkFile:
+        def __init__(self):
+            self.stdin = None
+            self.stdout = None
+            self.channel = None
+
+        def __call__(self, flags: str):
+            if "wb" == flags:
+                self.stdin = mock.Mock()
+                self.stdin.channel = self.channel
+                return self.stdin
+            elif "rb" == flags:
+                self.stdout = FakeFileStream(*stdout_src)
+                self.stdout.channel = self.channel
+                return self.stdout
+            raise ValueError("Unexpected flags: {!r}".format(flags))
+
+    return MkFile()
+
+
+@pytest.fixture
+def ssh_intermediate_channel(paramiko_ssh_client):
+    chan = mock.Mock(name="intermediate_channel")
+    transport = mock.Mock()
+    transport.attach_mock(chan, "open_channel")
+    get_transport = mock.Mock(return_value=transport)
+    _ssh = mock.Mock()
+    _ssh.attach_mock(get_transport, "get_transport")
+    paramiko_ssh_client.return_value = _ssh
+    return chan
+
+
+@pytest.fixture
+def ssh_transport(mocker):
+    transport = mock.Mock(name="transport")
+    mocker.patch("paramiko.Transport", return_value=transport)
+    return transport
+
+
+@pytest.fixture
+def ssh_transport_channel(chan_makefile, ssh_transport):
+    chan = mock.Mock(makefile=chan_makefile, closed=False)
+    chan_makefile.channel = chan
+    chan.attach_mock(mock.Mock(return_value=FakeFileStream(*stderr_src)), "makefile_stderr")
+
+    chan.configure_mock(exit_status=0)
+
+    chan.status_event.attach_mock(mock.Mock(return_value=True), "is_set")
+    open_session = mock.Mock(return_value=chan)
+    ssh_transport.attach_mock(open_session, "open_session")
+    return chan
+
+
+@pytest.fixture
+def ssh_auth_logger(mocker):
+    return mocker.patch("exec_helpers.ssh_auth.logger")
+
+
+@pytest.fixture
+def get_logger(mocker):
+    return mocker.patch("logging.getLogger")
+
+
+@pytest.fixture
+def ssh(
+    paramiko_ssh_client, ssh_intermediate_channel, ssh_transport_channel, auto_add_policy, ssh_auth_logger, get_logger
+):
+    return exec_helpers.SSHClient(host=host, port=port, auth=exec_helpers.SSHAuth(username=username, password=password))
+
+
+@pytest.fixture
+def exec_result():
+    return exec_helpers.ExecResult(cmd=command, stdin=None, stdout=stdout_src, stderr=stderr_src, exit_code=0)
+
+
+def teardown_function(function):
+    """Clean-up after tests."""
+    with mock.patch("warnings.warn"):
+        exec_helpers.SSHClient._clear_cache()
+
+
+def test_01_execute_through_host_no_creds(ssh, ssh_transport, exec_result) -> None:
+    target = "127.0.0.2"
+    result = ssh.execute_through_host(target, command)
+    ssh_transport.assert_has_calls(
+        (mock.call.connect(password=password, pkey=None, username=username), mock.call.open_session())
+    )
+    assert exec_result == result
+
+
+def test_02_execute_through_host_with_creds(ssh, ssh_transport, exec_result) -> None:
+    target = "127.0.0.2"
+    username_2 = "user2"
+    password_2 = "pass2"
+    result = ssh.execute_through_host(
+        target, command, auth=exec_helpers.SSHAuth(username=username_2, password=password_2)
+    )
+    ssh_transport.assert_has_calls(
+        (mock.call.connect(password=password_2, pkey=None, username=username_2), mock.call.open_session())
+    )
+    assert exec_result == result
+
+
+def test_03_execute_get_pty(ssh, ssh_transport_channel) -> None:
+    target = "127.0.0.2"
+    ssh.execute_through_host(target, command, get_pty=True)
+    ssh_transport_channel.get_pty.assert_called_with(term="vt100", width=80, height=24, width_pixels=0, height_pixels=0)
