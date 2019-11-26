e0c75e557e93793a9db8cc58fd5114e23e4b22a3
Aleksei Stepanov <penguinolog@gmail.com>
2019-11-26 10:59:15 +0100

    temporary: do not build i686 - issue in manylinux1

--- tools/run_docker.sh
+++ tools/run_docker.sh
@@ -2,6 +2,7 @@
 package_name="$1"
 if [ -z "$package_name" ]
 then
+    # shellcheck disable=SC2210
     &>2 echo "Please pass package name as a first argument of this script ($0)"
     exit 1
 fi
@@ -10,13 +11,13 @@ manylinux1_image_prefix="quay.io/pypa/manylinux1_"
 dock_ext_args=""
 declare -A docker_pull_pids=()  # This syntax requires at least bash v4
 
-for arch in x86_64 i686
+for arch in x86_64 # i686
 do
     docker pull "${manylinux1_image_prefix}${arch}" &
     docker_pull_pids[$arch]=$!
 done
 
-for arch in x86_64 i686
+for arch in x86_64 # i686
 do
     echo
     echo
@@ -26,7 +27,7 @@ do
     [ $arch == "i686" ] && dock_ext_args="linux32"
 
     echo Building wheel for $arch arch
-    docker run --rm -v `pwd`:/io "${manylinux1_image_prefix}${arch}" $dock_ext_args /io/tools/build-wheels.sh "$package_name"
+    docker run --rm -v "$(pwd):/io ${manylinux1_image_prefix}${arch} $dock_ext_args /io/tools/build-wheels.sh $package_name"
 
     dock_ext_args=""  # Reset docker args, just in case
 done
